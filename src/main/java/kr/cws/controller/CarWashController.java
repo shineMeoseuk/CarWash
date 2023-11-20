dbe9842e99d61ae284ffd81edfa68b9df5a789d6
Shine Meoseuk <112597600+shineMeoseuk@users.noreply.github.com>
2023-11-21 00:41:48 +0900

    예약하기 기능 구현 (#12)

--- src/main/java/kr/cws/controller/CarWashController.java
+++ src/main/java/kr/cws/controller/CarWashController.java
@@ -4,9 +4,12 @@ import javax.validation.Valid;
 import kr.cws.annotation.CurrentUserId;
 import kr.cws.annotation.LoginCheck;
 import kr.cws.model.dto.request.CarWashReq;
+import kr.cws.model.dto.request.ReservationReq;
 import kr.cws.service.CarWashService;
+import kr.cws.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarWashController {
 
     private final CarWashService carWashService;
+    private final ReservationService reservationService;
 
     /**
      * 세차장 등록하기.
@@ -38,5 +42,18 @@ public class CarWashController {
     public void registerCarWash(@CurrentUserId Long userId,
         @Valid @RequestBody CarWashReq carWashReq) {
         carWashService.registerCarWash(userId, carWashReq);
+    }
+
+    /**
+     * 예약하기.
+     *
+     * @since 1.0.0
+     */
+    @PostMapping("/{carWashId}/zones/{zoneId}/reservations")
+    //@LoginCheck
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createReservation(@CurrentUserId Long userId, @PathVariable("zoneId") Long zoneId,
+        @Valid @RequestBody ReservationReq reservationReq) {
+        reservationService.createReservation(reservationReq, userId, zoneId);
     }
 }
